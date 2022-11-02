f4685cb36f769467ccbb18ba41b68d39dc506714
midianet <midianet@gmail.com>
2022-11-02 10:27:56 -0300

    Finalizacao Menu

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,25 @@
 import { Button } from '@mui/material';
+import { useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { useDrawerContext } from '../shared/contexts';
 
 export const AppRoutes = () =>  {
 
-  const { toggleDrawerOpen } = useDrawerContext();
+  const { toggleDrawerOpen, setDrawerOptions } = useDrawerContext();
+  
+  useEffect(() => {
+    setDrawerOptions([
+      {
+        icon: 'home',
+        path: '/pagina-inicial',
+        label: 'Página Inicial'
+      }
+    ]);
+  },[]); 
 
 
   return (<Routes>
-    <Route path="/pagina-inicial" element={ 
-      <Button variant="contained" color='primary' onClick={toggleDrawerOpen}>Mudar Drawer</Button>
-    }/>
+    <Route path="/pagina-inicial" element={<Button variant="contained" color='primary' onClick={toggleDrawerOpen}>Mudar Drawer</Button>}/>
     <Route path="*" element={<Navigate to={'/pagina-inicial'} />} />
   </Routes>
   );
--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,5 +1,34 @@
 import { Avatar, Box, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme } from '@mui/material';
+import { useMatch, useNavigate, useResolvedPath } from 'react-router-dom';
 import { useDrawerContext } from '../../contexts';
+
+
+interface IListItemLinkProps {
+   to: string;
+   icon: string;
+   label: string;
+   onClick: (() => void) | undefined;
+}
+const ListItemLink: React.FC<IListItemLinkProps> = ({to, icon, label, onClick}) => {
+  const navigate = useNavigate();
+
+  const resolvedPath = useResolvedPath(to);
+  const match = useMatch({path: resolvedPath.pathname, end: false});
+
+  const handleClick = () => {
+    navigate(to);
+    onClick?.();
+  };
+
+  return (
+    <ListItemButton selected={!!match} onClick={handleClick} >
+      <ListItemIcon>
+        <Icon>{icon}</Icon>
+      </ListItemIcon>
+      <ListItemText primary={label}/>
+    </ListItemButton>
+  );
+};
 
 interface IDrawerProviderProps{
     children: React.ReactNode
@@ -7,7 +36,7 @@ interface IDrawerProviderProps{
 export const MenuLateral: React.FC<IDrawerProviderProps> = ({ children }) => {
   const theme = useTheme();
   const smDown = useMediaQuery(theme.breakpoints.down('sm'));
-  const { isDrawerOpen, toggleDrawerOpen } = useDrawerContext();
+  const { isDrawerOpen, toggleDrawerOpen, drawerOptions } = useDrawerContext();
   return (
     <>
       <Drawer open={isDrawerOpen} variant={smDown ? 'temporary' : 'permanent'} onClose={toggleDrawerOpen}>
@@ -20,12 +49,16 @@ export const MenuLateral: React.FC<IDrawerProviderProps> = ({ children }) => {
           <Divider />
           <Box flex={1}>
             <List component="nav">
-              <ListItemButton>
-                <ListItemIcon>
-                  <Icon>home</Icon>
-                </ListItemIcon>
-                <ListItemText primary="Página Inicial" />
-              </ListItemButton>
+              {drawerOptions.map(drawerOption => (
+                <ListItemLink 
+                  key={drawerOption.path}
+                  icon={drawerOption.icon}
+                  to={drawerOption.path}
+                  label={drawerOption.label}
+                  onClick={smDown ? toggleDrawerOpen: undefined}
+                />
+              ))}
+
             </List>
           </Box>
         </Box>
