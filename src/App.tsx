69c239f22fc74c4e050c550603da3f696b644568
midianet <midianet@gmail.com>
2022-11-02 00:33:30 -0300

    Adicionando e corrigindo Lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 
-import { BrowserRouter } from "react-router-dom";
-import { AppRoutes } from "./routes";
-import { AppThemeProvider } from "./shared/contexts/ThemeContext";
+import { BrowserRouter } from 'react-router-dom';
+import { AppRoutes } from './routes';
+import { AppThemeProvider } from './shared/contexts/ThemeContext';
 
 export const App = () => {
   return (
