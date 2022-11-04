bc40c81b26aa87ba7c2af030e8d6a41319c7f352
Marcos Fernando <midianet@gmail.com>
2022-11-04 20:16:09 -0300

    formulario de edicao

--- src/pages/pessoas/DetalhePessoa.tsx
+++ src/pages/pessoas/DetalhePessoa.tsx
@@ -1,37 +1,82 @@
+import { LinearProgress } from '@mui/material';
+import { Form } from '@unform/web';
+import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { BarraAcoesEdicao } from '../../shared/components';
+import { VTextField } from '../../shared/forms';
 import { LayoutBase } from '../../shared/layouts';
+import { PessoasService } from '../../shared/services/api/pessoas/PessoasService';
 
 export const DetalhePessoa: React.FC = () => {
   const { id = 'nova' } = useParams<'id'>();
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [nome, setNome] = useState('');
+  
+  useEffect(() => {
+    if(id !== 'nova') {
+      setIsLoading(true);      
+      PessoasService.getById(Number(id))
+        .then((result) => {
+          setIsLoading(false);
+          if (result instanceof Error) {
+            alert(result.message);
+            navigate('/pessoas');
+          }else{
+            setNome(result.nome);
+            console.log(result);
+          }
+        });
+    }
+  }, [id]);
 
   const handleSave = () => {
     console.log('save');
-  }
+  };
 
-  const handleDelete = () => {
-    console.log('delete');
-  }
+  const handleDelete = (id: number) => {
+    if (confirm('Realmente Deseja Apagar?')){
+      PessoasService.deleteById(id)
+        .then(result => {
+          if(result instanceof Error){
+            alert(result.message);
+          }else{
+            alert('Registro apagado com sucesso!');
+            navigate('/pessoas');
+          }
+        });
+    }
+  };
     
   return (
     <LayoutBase 
-      titulo='Detalhe de pessoa'
+      titulo={id === 'nova' ? 'Nova Pessoa' : nome}
       toolbar={
         <BarraAcoesEdicao
           rotuloNovo='Nova'
           mostrarNovo={id !== 'nova'}
           mostrarDeletar={id !== 'nova'}
           mostrarSalvar
+          prontoSalvar={!isLoading}
+          prontoNovo={!isLoading}
+          prontoDeletar={!isLoading}
           eventoNovo = {() => navigate('/pessoas/detalhe/nova')}
           eventoVoltar = {() => navigate('/pessoas')}
           eventoSalvar = {handleSave}
-          eventoDeletar = {handleDelete}
+          eventoDeletar = {() => handleDelete(Number(id))}
         />
       }
     >
-      <p>Detalhe Pessoa {id}</p>
+      {isLoading &&(
+        <LinearProgress variant='indeterminate' />
+      )}
+      <Form onSubmit={console.log} >
+        <VTextField 
+          name='nome'
+          
+        />
+      </Form>
     </LayoutBase>
-
   );
+
 };
